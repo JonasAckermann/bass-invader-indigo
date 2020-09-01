c37802bcd352f867955f5254b47ec589e76335d9
JonasAckermann <jonas.p.ackermann@gmail.com>
2020-09-01 22:48:16 +0200

    merge

---
+++ src/main/scala/model/Location.scala
@@ -0,0 +1,18 @@
+package model
+
+import indigo._
+
+/**
+  * Like Point, but using Doubles
+  *
+  * @param x
+  * @param y
+  */
+case class Location(x: Double, y: Double){
+    def moveTo(location: Location): Location = Location(location.x, location.y)
+    def moveBy(x: Double, y: Double): Location = Location(this.x + x, this.y + y)
+    def toPoint: Point = Point(Math.round(x).toInt, Math.round(y).toInt)
+}
+object Location {
+    def fromPoint(point: Point): Location = Location(point.x.toDouble, point.y.toDouble)
+}
